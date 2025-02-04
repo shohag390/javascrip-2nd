64180f2278f269898cf87af1c81e487717c4a9db
shohag390 <shohag390@users.noreply.github.com>
2025-02-05 00:26:58 +0600

    Variable add

---
+++ script.js
@@ -0,0 +1,25 @@
+// Number Type Variable 
+var age1 = 25;
+console.log(age1);
+
+var age2 = 23;
+console.log(typeof age2);
+console.log(age1 + age2);
+console.log(age1 - age2);
+console.log(age1 * age2);
+console.log(age1 % age2);
+console.log(age1 / age2);
+
+
+// String Type variable
+var myName = "Md Shohag Hossen"
+
+console.log(myName);
+console.log(typeof myName);
+
+
+// Boolan Type variable 
+var isRice = true;
+console.log(isRice);
+console.log(typeof isRice);
+
